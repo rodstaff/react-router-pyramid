58491e84a2b7d2166f3a7daffb362a8c07c1d04d
rodk <rod2010rod2010@hotmail.com>
2016-09-17 10:06:25 -0400

    corrected some Redux actions

--- dev/js/components/layout.js
+++ dev/js/components/layout.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchUser} from '../actions/userActions'
+import {fetchUser, setUserName, setUserAge} from '../actions/userActions'
 
 //  To enable @connect, install 'babel-plugin-transform-decorators-legacy'.
 //  It will inject props (i.e. the return values) into in the 'layout' component when it is loaded.
 @connect((store) => {    
   return {               
     user: store.userA.user,
-    initialUser: store.userA.users,   // 'test' => can be called anything, e.g. 'userList'
+    initialUser: store.userA.users,   // 'initialUser' => can be called anything, e.g. 'userList'
     tweet: store.tweetsZ.tweets
   };
 })
@@ -15,6 +15,8 @@ import {fetchUser} from '../actions/userActions'
 export default class Layout extends React.Component {
   componentWillMount() {
     this.props.dispatch(fetchUser())
+    this.props.dispatch(setUserName("Henri")),
+    this.props.dispatch(setUserAge(3005))
   }
   render() {
     console.log(this.props);
@@ -22,7 +24,7 @@ export default class Layout extends React.Component {
   	  <div>
         <h1>Redux-React: Pyramids and Turtles!!</h1>
         <h3>{this.props.user.name}, Age: {this.props.user.age}</h3>
-        <h3>Ninja turtle {this.props.initialUser[0]} sent a tweet: {this.props.tweet[2]}</h3>
+        <h3>Ninja turtle {this.props.initialUser[3]} sent a tweet: {this.props.tweet[1]}</h3>
         <h4>Chto Eto?</h4>
       </div>
   	);
